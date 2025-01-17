671f4998a860846188e6daf6f05315470ccd5ff7
Yoomin Hwang <yoominh3@gmail.com>
2025-01-18 01:21:55 +0900

    Chore: develop env. setting

---
+++ src/main/java/com/purification/cap2ass1/Cap2ass1Application.java
@@ -0,0 +1,13 @@
+package com.purification.cap2ass1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap2ass1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap2ass1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/purification/cap2ass1/Cap2ass1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purification.cap2ass1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap2ass1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
